5122828dfea45df0480e341e80f9e4f76e28a683
zeel sheladiya <zeelsheladiya52@gmail.com>
2022-11-12 21:03:52 -0500

    Update main.py

--- main.py
+++ main.py
@@ -5,6 +5,15 @@ dpg.create_context()
 with dpg.window(tag="Primary Window"):
     dpg.add_text("Hello, world")
 
+    button1 = dpg.add_button(label="Press Me!")
+
+    slider_int = dpg.add_slider_int(label="Slide to the left!", width=100)
+    slider_float = dpg.add_slider_float(label="Slide to the right!", width=100)
+
+    dpg.add_simple_plot(label="Simpleplot1", default_value=(0.3, 0.9, 0.5, 0.3), height=300)
+    dpg.add_simple_plot(label="Simpleplot2", default_value=(0.3, 0.9, 2.5, 8.9), overlay="Overlaying", height=180,
+                        histogram=True)
+
 dpg.create_viewport(title='Custom Title', width=600, height=200)
 dpg.setup_dearpygui()
 dpg.show_viewport()
