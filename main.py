84dd805e6f0ac20b475a61eed9426c67fb0919b1
zeel sheladiya <zeelsheladiya52@gmail.com>
2022-11-13 19:54:33 -0500

    Update main.py

--- main.py
+++ main.py
@@ -2,10 +2,13 @@ import dearpygui.dearpygui as dpg
 
 dpg.create_context()
 
+def print_text():
+    print("yoyo")
+
 with dpg.window(tag="Primary Window"):
     dpg.add_text("Hello, world")
 
-    button1 = dpg.add_button(label="Press Me!")
+    button1 = dpg.add_button(label="Press Me!", callback=print_text)
 
     slider_int = dpg.add_slider_int(label="Slide to the left!", width=100)
     slider_float = dpg.add_slider_float(label="Slide to the right!", width=100)
