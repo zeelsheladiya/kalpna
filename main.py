7e65cc5c1d54e12ab2831bd8d3b4e2b6daaedb91
zeel sheladiya <zeelsheladiya52@gmail.com>
2022-11-22 13:11:50 -0500

    Update main.py

--- main.py
+++ main.py
@@ -10,13 +10,9 @@ with dpg.window(tag="Primary Window"):
 
     button1 = dpg.add_button(label="Press Me!", callback=print_text)
 
-    slider_int = dpg.add_slider_int(label="Slide to the left!", width=100)
-    slider_float = dpg.add_slider_float(label="Slide to the right!", width=100)
-
     dpg.add_simple_plot(label="Simpleplot1", default_value=(0.3, 0.9, 0.5, 0.3), height=300)
-    dpg.add_slider_int(label="slider")
 
-dpg.create_viewport(title='Custom Title', width=600, height=200)
+dpg.create_viewport(title='Custom Title', width=800, height=800)
 dpg.setup_dearpygui()
 dpg.show_viewport()
 dpg.set_primary_window("Primary Window", True)
