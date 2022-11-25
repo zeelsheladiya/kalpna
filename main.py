bd483e7bf1eefd22a12a046fd7332422af0cdebc
zeel sheladiya <zeelsheladiya52@gmail.com>
2022-11-25 06:05:55 -0500

    update script

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
 import dearpygui.dearpygui as gui
+import sys
+
+sys.getdefaultencoding()
 
 gui.create_context()
+
 
 # event section
 def file_browse_callback(sender, _, user_data):
@@ -10,23 +14,28 @@ def file_browse_callback(sender, _, user_data):
 
 # tab section
 def init_main_tab():
-    gui.add_combo(["CSV", "Excel"], default_value="CSV", width=250, tag="plot", label="Select File Type")
 
-    gui.add_button(label="Browse", callback=file_browse_callback, user_data="btn_browse")
+    # file type sector
+    gui.add_combo(["CSV", "Excel"], default_value="CSV", width=400, tag="plot", label="Select File Type")
 
-    with gui.plot(label="plot", height=300, width=400):
-        pass
+    # browse button
+    gui.add_button(label="Browse", callback=file_browse_callback, user_data="btn_browse", width=200)
+
+    # file selection section
+    gui.add_input_text(enabled=False)
+    gui.add_text("File Not Selected", color=(255, 0, 0))
 
 
-with gui.window(tag="Primary Window"):
+with gui.window(tag="primary_window"):
     with gui.tab_bar():
-        with gui.tab(label="Main"):
+        with gui.tab(label="main_tab"):
             init_main_tab()
 
 
-gui.create_viewport(title='Kalpna', width=800, height=500)
+gui.create_viewport(title='Kalpna')
 gui.setup_dearpygui()
 gui.show_viewport()
-gui.set_primary_window("Primary Window", True)
+gui.maximize_viewport()
+gui.set_primary_window("primary_window", True)
 gui.start_dearpygui()
 gui.destroy_context()
