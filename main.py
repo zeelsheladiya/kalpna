ab96e86cff6a70c80ed40ac531ac43c83919c707
zeel sheladiya <zeelsheladiya52@gmail.com>
2022-11-25 13:04:45 -0500

    update script

--- main.py
+++ main.py
@@ -1,23 +1,38 @@
 import dearpygui.dearpygui as gui
-
+import easygui
+import pandas as pd
 
 gui.create_context()
 
+# global variable
+FILE_PATH = ""
+
+FILE_TYPE = {
+    "CSV": "csv",
+    "Excel": "xlsx"
+}
+
 
 # event section
-def file_browse_callback(sender, _, user_data):
+def btn_browse_file_browse_callback(sender, _, user_data):
+    global FILE_PATH
     print(f'sender:- {sender}')
     print(f'user data:- {user_data}')
+    file_type_name = gui.get_value("cb_file_type_main_tab")
+
+    FILE_PATH = easygui.fileopenbox(msg=f'Please locate the {file_type_name} file',
+                                    default=f"*.{FILE_TYPE[file_type_name]}",
+                                    filetypes=[f"*.{FILE_TYPE[file_type_name]}"])
 
 
 # tab section
 def init_main_tab():
-
     # file type sector
-    gui.add_combo(["CSV", "Excel"], default_value="CSV", width=400, tag="plot", label="Select File Type")
+    gui.add_combo(["CSV", "Excel"], default_value="CSV", width=400, tag="cb_file_type_main_tab",
+                  label="Select File Type")
 
     # browse button
-    gui.add_button(label="Browse", callback=file_browse_callback, user_data="btn_browse", width=200)
+    gui.add_button(label="Browse", callback=btn_browse_file_browse_callback, user_data="btn_browse", width=200)
 
     # file selection section
     gui.add_input_text(enabled=False)
@@ -28,7 +43,6 @@ with gui.window(tag="primary_window"):
     with gui.tab_bar():
         with gui.tab(label="main_tab"):
             init_main_tab()
-
 
 gui.create_viewport(title='Kalpna')
 gui.setup_dearpygui()
