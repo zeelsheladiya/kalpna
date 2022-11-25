d9865570998579d35e54c5727ad9b6863ae53680
zeel sheladiya <zeelsheladiya52@gmail.com>
2022-11-25 14:48:16 -0500

    Update main.py

--- main.py
+++ main.py
@@ -12,10 +12,12 @@ FILE_TYPE = {
     "Excel": "xlsx"
 }
 
+DATA_TABLE = pd.DataFrame()
+
 
 # event section
 def btn_browse_file_browse_callback(sender, _, user_data):
-    global FILE_PATH
+    global FILE_PATH, DATA_TABLE
     print(f'sender:- {sender}')
     print(f'user data:- {user_data}')
     file_type_name = gui.get_value("cb_file_type_main_tab")
@@ -24,6 +26,12 @@ def btn_browse_file_browse_callback(sender, _, user_data):
                                     default=f"*.{FILE_TYPE[file_type_name]}",
                                     filetypes=[f"*.{FILE_TYPE[file_type_name]}"])
 
+    DATA_TABLE = pd.read_csv(FILE_PATH)
+    print(DATA_TABLE)
+
+    gui.set_value("txt_file_log", FILE_PATH)
+    gui.set_value("txt_file_selected_log", "File is Selected")
+    # gui.add_color_value("txt_file_selected_log", (0, 255, 0))
 
 # tab section
 def init_main_tab():
@@ -35,8 +43,10 @@ def init_main_tab():
     gui.add_button(label="Browse", callback=btn_browse_file_browse_callback, user_data="btn_browse", width=200)
 
     # file selection section
-    gui.add_input_text(enabled=False)
-    gui.add_text("File Not Selected", color=(255, 0, 0))
+    gui.add_input_text(tag="txt_file_log", enabled=False)
+    gui.add_text("File Not Selected", tag="txt_file_selected_log", color=(255, 0, 0))
+
+    # table for visualization data
 
 
 with gui.window(tag="primary_window"):
@@ -44,7 +54,7 @@ with gui.window(tag="primary_window"):
         with gui.tab(label="main_tab"):
             init_main_tab()
 
-gui.create_viewport(title='Kalpna')
+gui.create_viewport(title='Kalpana')
 gui.setup_dearpygui()
 gui.show_viewport()
 gui.maximize_viewport()
