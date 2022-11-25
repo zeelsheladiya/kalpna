827640ade16cf6c2d2c5fa556b6df359b43b792d
zeel sheladiya <zeelsheladiya52@gmail.com>
2022-11-25 04:09:36 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,38 +1,32 @@
-import dearpygui.dearpygui as dpg
+import dearpygui.dearpygui as gui
 
-dpg.create_context()
+gui.create_context()
+
+# event section
+def file_browse_callback(sender, _, user_data):
+    print(f'sender:- {sender}')
+    print(f'user data:- {user_data}')
 
 
-def select_file():
-    return "zeel"
-
+# tab section
 def init_main_tab():
+    gui.add_combo(["CSV", "Excel"], default_value="CSV", width=250, tag="plot", label="Select File Type")
 
-    dpg.add_text("Kalpna")
+    gui.add_button(label="Browse", callback=file_browse_callback, user_data="btn_browse")
 
-    selected_file_type = dpg.add_combo(["CSV","Excel"],default_value="CSV",width=250, tag="plot", label="Select File Type")
-
-    dpg.add_button(label="Browse", callback=select_file())
-
-
-    with dpg.plot(label="Sine curve", height=300, width=400):
+    with gui.plot(label="plot", height=300, width=400):
         pass
 
 
-
-with dpg.window(tag="Primary Window"):
-
-    with dpg.tab_bar():
-
-        with dpg.tab(label="Main"):
-
+with gui.window(tag="Primary Window"):
+    with gui.tab_bar():
+        with gui.tab(label="Main"):
             init_main_tab()
 
 
-
-dpg.create_viewport(title='Custom Title', width=800, height=500)
-dpg.setup_dearpygui()
-dpg.show_viewport()
-dpg.set_primary_window("Primary Window", True)
-dpg.start_dearpygui()
-dpg.destroy_context()
+gui.create_viewport(title='Kalpna', width=800, height=500)
+gui.setup_dearpygui()
+gui.show_viewport()
+gui.set_primary_window("Primary Window", True)
+gui.start_dearpygui()
+gui.destroy_context()
