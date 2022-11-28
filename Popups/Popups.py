f3ad69d4d038ee0a90f2ed844e80bbd4123fe2b6
zeel sheladiya <zeelsheladiya52@gmail.com>
2022-11-28 09:30:43 -0500

    Update Popups.py

--- Popups/Popups.py
+++ Popups/Popups.py
@@ -1,5 +1,10 @@
 # popups =================================================================================================
 
 def basic_popup(gui=None, easygui=None, title: str = "", message: str = "", button_name: str = "OK"):
-    popup_output = easygui.msgbox(message, title, button_name)
-    print(f'Basic popup output:- {popup_output}')
+
+    try:
+        easygui.msgbox(message, title, button_name)
+        # popup_output = easygui.msgbox(message, title, button_name)
+        # print(f'Basic popup output:- {popup_output}')
+    except Exception:
+        pass
