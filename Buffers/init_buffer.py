28cf25c7f2ed1635e4090dbcad0659d8f9193e13
zeel sheladiya <zeelsheladiya52@gmail.com>
2022-12-05 08:54:36 -0500

    Update init_buffer.py

--- Buffers/init_buffer.py
+++ Buffers/init_buffer.py
@@ -1,4 +1,10 @@
 
 # init buffer
 def init_buffer(gui):
-    pass
+
+    # For table view in main page
+    gui.delete_item("main_tab_table_view")
+
+    # For node tab
+    gui.delete_item("node_tab")
+
