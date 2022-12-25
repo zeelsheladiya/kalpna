f2da4f3b7791e7a41a47f44ea836ab8ce92df822
zeel sheladiya <46935793+zeelsheladiya@users.noreply.github.com>
2022-12-25 15:21:41 -0500

    Update node_render.py

--- Node_tab/node_render.py
+++ Node_tab/node_render.py
@@ -42,10 +42,11 @@ def delink_callback(sender, app_data, gui):
 
 def init_node_menu(gui):
 
-    # TODO: need to solve this error to show combo box at node menu
+    # TODO: need to solve this error to show combo box at node menu or choose different option
     gui.add_combo({"CSV": "csv", "Excel": "xlsx"},
                   default_value="DataFrame", width=400, tag="cb_node_type_node_tab",
                   parent="right_click_menu_node_menu")
+
 
 # ============================================================================================
 # ============================================================================================
