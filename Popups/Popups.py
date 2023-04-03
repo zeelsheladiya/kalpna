449ce383f08135bb6e2e8720bbe5d8ebfddf2b73
zeel sheladiya <46935793+zeelsheladiya@users.noreply.github.com>
2023-04-02 21:27:56 -0400

    Update Popups.py

--- Popups/Popups.py
+++ Popups/Popups.py
@@ -1,23 +1,23 @@
 # popups =================================================================================================
 
-def basic_popup(gui=None, parent_window_name: str = "primary_window", title: str = "",
-                message: str = "", button_name: str = "OK", error: str = "Not defined"):
-
-    try:
-        main_width = gui.get_item_width(parent_window_name)
-        main_height = gui.get_item_height(parent_window_name)
-
-        with gui.window(label=title, modal=True, show=True, tag="modal_id"):
-            gui.add_text(message)
-            gui.add_text(error)
-            gui.add_separator()
-            # gui.add_checkbox(label="Don't ask me next time")
-            with gui.group(horizontal=True):
-                gui.add_button(label=button_name, width=150,
-                               callback=lambda: gui.configure_item("modal_id", show=False))
-
-        gui.set_item_pos("modal_id", (int(main_width / 3), int(0.4 * main_height)))
-
-    except Exception as e:
+# def basic_popup(gui=None, parent_window_name: str = "primary_window", title: str = "",
+#                 message: str = "", button_name: str = "OK", error: str = "Not defined"):
+#
+#     try:
+#         main_width = gui.get_item_width(parent_window_name)
+#         main_height = gui.get_item_height(parent_window_name)
+#
+#         with gui.window(label=title, modal=True, show=True, tag="modal_id"):
+#             gui.add_text(message)
+#             gui.add_text(error)
+#             gui.add_separator()
+#             # gui.add_checkbox(label="Don't ask me next time")
+#             with gui.group(horizontal=True):
+#                 gui.add_button(label=button_name, width=150,
+#                                callback=lambda: gui.configure_item("modal_id", show=False))
+#
+#         gui.set_item_pos("modal_id", (int(main_width / 3), int(0.4 * main_height)))
+#
+#     except Exception as e:
         # need it to something but not that important
         pass
