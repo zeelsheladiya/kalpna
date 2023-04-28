f7ace89cc0a74218f61c1f5ace844823443893e1
zeel sheladiya <46935793+zeelsheladiya@users.noreply.github.com>
2023-04-27 22:14:16 -0400

    Update main.py

--- main.py
+++ main.py
@@ -3,10 +3,10 @@ import pandas as pd
 
 # internal file importing ======================================================================
 
-# from Popups.Popups import *
-# from Msg_Str.msg_str import *
-# from Main_tab.table_rander import *
-# from Node_tab.node_render import *
+from Popups.Popups import *
+from Msg_Str.msg_str import *
+from Main_tab.table_rander import *
+from Node_tab.node_render import *
 
 # global variable ============================================================================
 
