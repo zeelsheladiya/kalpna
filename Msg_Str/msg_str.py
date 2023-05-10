9b70f19f5a796ad430ff8055279ef5cf171a642e
zeel sheladiya <46935793+zeelsheladiya@users.noreply.github.com>
2023-05-09 20:32:55 -0400

    Update msg_str.py

--- Msg_Str/msg_str.py
+++ Msg_Str/msg_str.py
@@ -1,12 +1,3 @@
 
 # main tab msg strings
 
-error_msg_title: str = "Error"
-
-file_not_support_msg_str: str = "This File Format is not Supported."
-
-# node tab msg strings
-
-duplicate_column_node_error_title: str = "Duplicate Node Error"
-
-duplicate_column_node_error: str = "This Column node is already exist in the ground."
